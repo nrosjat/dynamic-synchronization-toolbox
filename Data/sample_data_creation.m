5bed4d37607417a804182943b805c207105d109b
nrosjat <34684014+nrosjat@users.noreply.github.com>
2022-05-11 11:56:20 +0200

    Add files via upload

--- Data/sample_data_creation.m
+++ Data/sample_data_creation.m
@@ -1,3 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Script for creating artificial training data 
+%
+% The script will create two identical subjects with dimensions                 
+%   output: eegData = [61 channels, 48 frequencies, 800 timepoints, 100 trials]
+%   Stored as 'Sub01/Sub01.mat' and 'Sub02/Sub02.mat'
+%
+% A set of 27 channels is perfectly phase-locked after artifical
+% stimulation in the time-interval [300:500] for trials 1:25 and 51:75.
+%
+% In contrast, remaining channels are perfectly phase-locked in the time-interval
+% [300:500] for trials 26:50 and 76:100.
+%
+% Output: eegData
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 time = -1500:5:2495;
 
 eegData = zeros(61,48,800,100);
@@ -63,3 +79,5 @@ for ch= 1:61
     end
 end
 
+save('Sub01/Sub01_eeg.mat','eegData')
+save('Sub02/Sub02_eeg.mat','eegData')
